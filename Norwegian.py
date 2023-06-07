b50ffd2b324e407dd9441c58334c832bea95f5bd
Maksym Malichenko <92025611+Malichenko-Maksym@users.noreply.github.com>
2023-06-07 21:26:01 +0200

    Update Norwegian.py

--- Norwegian.py
+++ Norwegian.py
@@ -7,7 +7,7 @@ def money_input(message):
         print(message)
         amount=input()
         try:
-            amount=int(amount)
+            amount=double(amount)
             if(amount>0):
                 bad_input_money=False
             else:
@@ -32,21 +32,21 @@ def year_input(message):
             print("must be a number")
     return year
 
-def mount_input(message):
+def month_input(message):
     bad_input_month=True
     while bad_input_month:
         print(message)
-        mount=input()
+        month=input()
         try:
-            mount=int(mount)
-            if(1<=mount and mount<=12):
+            month=int(month)
+            if(1<=month and month<=12):
                 bad_input_month=False
             else:
                 print("There are only 12 of them...")
         except:
             print("must be a number")
-    return mount
-
+    return month
+#function that formatting inputed data to be comparable with data in file 
 def data_format(year,month):
     if(month//10==0):
         return str(year)+"M0"+str(month)
@@ -60,18 +60,19 @@ old_payment=money_input("How much you have been paying before?")
 new_payment=money_input("How much you pay now?")
 
 
-
+#
 incorrect_input=True
 while incorrect_input:
     #year input
     year_old_payment=year_input("When housing payment was set(only year)")
-    month_old_payment=mount_input("When housing payment was set(only month)")
+    month_old_payment=month_input("When housing payment was set(only month)")
     year_new_payment=year_input("When housing payment was changed(only year)")
-    mount_new_payment=mount_input("When housing payment was changed(only month)")
-    
+    month_new_payment=month_input("When housing payment was changed(only month)")
+# data is provided with a small delay, so we have to check, if we have a data, user asking us about 
     if year_new_payment==datetime.date.today().year and mount_new_payment>=datetime.date.today().month-1:
         print("We don't have data yet")
         input("Press Enter to continue")
+# checking, if data provided by user is adequate 
     else:
         if year_old_payment<year_new_payment or (year_old_payment==year_new_payment and month_old_payment<=mount_new_payment ):
             incorrect_input=False
@@ -80,13 +81,17 @@ while incorrect_input:
             input("Press Enter to continue")
             
 
-# we will count in months
+# we have to find how much time passed since last payment setting 
+#we will count in months
 old_payment_months= year_old_payment*12+month_old_payment
 new_payment_months= year_new_payment*12+mount_new_payment
 
 
 if(new_payment_months-old_payment_months>=12):
-    
+  # data is contained in a file with a certain format of records
+# we will iterate file completely, and after finding "starter" (old_payment_date)
+# we will start summing inflation rate
+# after finding "stopper" (new_payment_date) we will finish summing inflation 
     old_payment_date=data_format(year_old_payment,month_old_payment)
     new_payment_date=data_format(year_new_payment,mount_new_payment)
     allowed_to_pay=old_payment
